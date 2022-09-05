0edb593281296fa607c6d61dd4ff741256e1f9c0
Jack Leong <99371792+JackTheLime@users.noreply.github.com>
2022-09-06 10:20:01 +1200

    Term, Week 7, Tuesday
    Decided to finsish website here. Pretty much Sir said I can explain my design processes via the three pages I already have. Its been real fun but its over now!

--- js/Homepage.js
+++ js/Homepage.js
@@ -10,3 +10,21 @@ $('.carousel').carousel({
   interval: 2
 })
 
+
+
+$(document).ready(function() {
+
+    $("input.slider").on("input change", function(event) {
+        var element = $(this).parents("div.before-after-slider ");
+        var pos = event.target.value;
+
+        element.find("div.before").css({width: pos + "%"});
+        element.find("div.slider-button").css({left: "calc(" + pos + "% - 18px)"});
+    });
+
+})
+
+var element = $(this).parents("div.container");
+var pos = event.target.value;
+element.find("div.before").css({width: pos + "%"});
+element.find("div.slider-button").css({left: "calc(" + pos + "% - 18px)"});
